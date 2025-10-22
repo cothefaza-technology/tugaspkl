f6e6f4bec5fce523e3157baf3dc2c4f60c292e3e
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2025-10-22 07:42:00 +0700

    alan

--- alan/tugas_keempat.php
+++ alan/tugas_keempat.php
@@ -26,5 +26,25 @@
 </form>
 
 
+       <table class="table">
+       <thead>
+       <tr>
+       <th scope="col">No</th>
+       <th scope="col">NIS</th>
+       <th scope="col">Nama</th>
+       <th scope="col">Kelas</th>
+       </tr>
+       </thead>
+       <tbody>
+       <tr>
+       <th scope="row">1</th>
+       <td><?=$data['nis']?></td>
+       <td><?=$data['nama']?></td>
+       <td><?=$data['kelas']?></td>
+       </tr>
+    
+       </tbody>
+       </table>
+      
 <?php
 ?>
