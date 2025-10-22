01ada0e72c1c387a45ac740f5ff7ba0a5e6a14be
diarawr <itsmediarr@gmail.com>
2025-10-22 08:11:14 +0700

    fina

--- afina/tugas-empat.php
+++ afina/tugas-empat.php
@@ -21,4 +21,30 @@ if($qry == true){
     <input type="text" name="kelas">
     <button type="submit">Submit</button>
 </form>
+
 <table class="table">
+<thead>
+<tr>
+<th scope="col">No</th>
+<th scope="col">NIS</th>
+<th scope="col">Nama</th>
+<th scope="col">Kelas</th>
+</tr>
+</thead>
+<tbody>
+<?php
+     $no = 1;
+     $sqlResult = $conn->query("SELECT*FROM tb_siswa");
+     foreach ($sqlResult as $data) {
+?>
+<tr>
+<th scope="row">1</th>
+<td><?=$data['nis']?></td>
+<td><?=$data['nama']?></td>
+<td><?=$data['kelas']?></td>
+</tr>
+<?php
+        }
+?>
+</tbody>
+</table>
