60193dc9c3dd1974d8e8de2d8a32606cf8f81cf6
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2025-10-23 08:17:24 +0700

    alan

--- alan/tugas_kelima.php
+++ alan/tugas_kelima.php
@@ -1,13 +1,69 @@
+<?php
+     include "koneksi.php";
+   if(isset($_POST['btn'])){
+       $a = $_POST['kd_barang'];
+       $b = $_POST['nama_brg'];
+       $c = $_POST['harga_brg'];
+       $d = $_POST['jumlah_brg'];
+       $e = $_POST['kondisi_brg'];
+        $qry = $conn->query("INSERT INTO 
+        tb_barang(kd_barang,nama_brg,harga_brg,jumlah_brg,kondisi_brg) VALUES ('$a','$b','$c','$d','$e')");
+    
+   if($qry == true){
+        echo"<script>alert('Data Berhasil diinput....')</script>";
+   }else{
+        echo"<script>alert('Data gagal diinput....')</script>";
+     }
+   }
+
+   ?> 
+ 
  <form method="post">       
     <label>kode barang</label>
-    <input type="text" name="nis"><br>
+    <input type="number" name="kode"><br>
     <label>Nama barang</label>
     <input type="text" name="nama"><br>
     <label>harga barang</label>
-    <input type="text" name="kelas"><br>
+    <input type="number" name="harga"><br>
     <label>jumlah barang</label>
-    <input type="text" name="kelas"><br>
+    <input type="number" name="jumlah"><br>
     <label>kondisi barang</label>
-    <input type="text" name="kelas"><br>
+    <input type="text" name="kondisi"><br>
     <button type="submit" name="btn">Submit</button>
 </form>
+
+
+       <table class="table">
+       <thead>
+       <tr>
+       <th scope="col">No</th>
+       <th scope="col">kode barang</th>
+       <th scope="col">nama barang</th>
+       <th scope="col">harga barang</th>
+       <th scope="col">jumlah barang</th>
+       <th scope="col">kondisi barang</th>
+       </tr>
+       </thead>
+       <tbody>
+          <?php
+          $no = 1;
+          $sqlResult = $conn->query("SELECT*FROM tb_barang");
+          foreach($sqlResult as $data){
+            ?>
+       <tr>
+       <td><?=$no++?></td>
+       <td><?=$data['kd_barang']?></td>
+       <td><?=$data['nama_brg']?></td>
+       <td><?=$data['harga_brg']?></td>
+       <td><?=$data['jumlah_brg']?></td>
+       <td><?=$data['kondisi_brg']?></td>
+       </tr>
+       
+    
+      
+<?php
+        }
+?>
+ </tbody>
+       </table>
+      
