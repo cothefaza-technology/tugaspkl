025b8df7db7ec9d6ba509ea3216599e12b8651fd
Alan raja zulkifli <alanrajazulkifli@gmail.com>
2025-10-23 07:44:21 +0700

    alan

---
+++ alan/tugas_kelima.php
@@ -0,0 +1,13 @@
+ <form method="post">       
+    <label>kode barang</label>
+    <input type="text" name="nis"><br>
+    <label>Nama barang</label>
+    <input type="text" name="nama"><br>
+    <label>harga barang</label>
+    <input type="text" name="kelas"><br>
+    <label>jumlah barang</label>
+    <input type="text" name="kelas"><br>
+    <label>kondisi barang</label>
+    <input type="text" name="kelas"><br>
+    <button type="submit" name="btn">Submit</button>
+</form>
